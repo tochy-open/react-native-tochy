27f59fefed7bf5fc7375e4c96a9335c54a7fcc09
tochy <tochy@openrhapsody.com>
2023-11-01 19:08:27 +0900

    unused

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,24 @@
 import { NativeModules, Platform } from 'react-native';
 
-export * from './banner'
+
+export * from './banner';
 
 const LINKING_ERROR =
   `The package 'adrop-ads-react-native' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  Platform.select({ ios: '- You have run \'pod install\'\n', default: '' }) +
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo Go\n';
 
 const AdropAds = NativeModules.AdropAds
-  ? NativeModules.AdropAds
-  : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR);
-        },
-      }
-    );
+                 ? NativeModules.AdropAds
+                 : new Proxy(
+    {},
+    {
+      get() {
+        throw new Error(LINKING_ERROR);
+      },
+    },
+  );
 
 export function initialize(production: boolean): Promise<void> {
   return AdropAds.initialize(production);
